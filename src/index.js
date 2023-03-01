f21de64dddd1d4f39d52f620eedc75f8a0b43caf
Pablo Paixão <zkyouta@hotmail.com>
2023-03-01 12:31:09 -0300

    Feat: List Thermals

--- src/index.js
+++ src/index.js
@@ -1,36 +1,42 @@
 const ThermalPrinter = require('node-thermal-printer').printer;
 const PrinterTypes = require('node-thermal-printer').types;
+const nodePrinter = require('@flovy/node-printer');
 
-let printer = null;
-try {
-  printer = new ThermalPrinter({
-    type: PrinterTypes.TANCA,
-    interface: 'printer:tanquinha',
-    driver: 'printer',
-  });
-} catch (e) {
-  console.log('Failed to try to print');
+const listPrinters = nodePrinter.getPrinters();
+
+for (const printer of listPrinters) {
+  console.log(printer);
 }
 
-if (!printer) return;
+// let printer = null;
+// try {
+//   printer = new ThermalPrinter({
+//     type: PrinterTypes.TANCA,
+//     interface: 'printer:tanquinha',
+//   });
+// } catch (e) {
+//   console.log('Failed to try to print');
+// }
 
-(async () => {
-  const isConnected = await printer.isPrinterConnected();
-  console.log('Status da impressora: ', isConnected);
+// if (!printer) return;
 
-  if (!isConnected)
-    return console.log(
-      'Impressora desligada, portanto não foi possível imprimir!'
-    );
+// (async () => {
+//   const isConnected = await printer.isPrinterConnected();
+//   console.log('Status da impressora: ', isConnected);
 
-  printer.alignCenter();
-  printer.println('Testing Print');
-  printer.cut();
+//   if (!isConnected)
+//     return console.log(
+//       'Impressora desligada, portanto não foi possível imprimir!'
+//     );
 
-  try {
-    await printer.execute();
-    console.error('Print Finish!');
-  } catch (error) {
-    console.log('Print Error:', error);
-  }
-})();
+//   printer.alignCenter();
+//   printer.println('Testing Print');
+//   printer.cut();
+
+//   try {
+//     await printer.execute();
+//     console.error('Print Finish!');
+//   } catch (error) {
+//     console.log('Print Error:', error);
+//   }
+// })();
