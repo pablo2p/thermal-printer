a3f0924b24ef5797581f633e6ce7e86a370bfdf8
Pablo Paixão <zkyouta@hotmail.com>
2023-03-01 12:40:16 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,47 +4,49 @@ const nodePrinter = require('@flovy/node-printer');
 
 const listPrinters = nodePrinter.getPrinters();
 
-for (const printer of listPrinters) {
-  const { shareName, driverName, portName } = printer || {};
-  if (shareName !== 'tanquinha') continue;
-  // Portname se começar com número é o ip
-  // USB PORT: -> é via usb
+(async () => {
+  for (const printer of listPrinters) {
+    const { shareName, driverName, portName } = printer || {};
+    if (shareName !== 'tanquinha') continue;
+    // Portname se começar com número é o ip
+    // USB PORT: -> é via usb
 
-  console.log(`Olha a ${shareName} feliz da vida!`);
+    console.log(`Olha a ${shareName} feliz da vida!`);
 
-  // not started with number
-  if (!s.match(/^\d/)) {
-    continue;
+    // not started with number
+    if (!s.match(/^\d/)) {
+      continue;
+    }
+
+    let printerConstructor = null;
+    try {
+      printerConstructor = new ThermalPrinter({
+        type: PrinterTypes.TANCA,
+        interface: `TCP://${portName}`,
+      });
+    } catch (e) {
+      console.log('Failed to try to print');
+    }
+
+    if (!printerConstructor) return;
+
+    const isConnected = await printerConstructor.isPrinterConnected();
+    console.log('Status da impressora: ', isConnected);
+
+    if (!isConnected)
+      return console.log(
+        'Impressora desligada, portanto não foi possível imprimir!'
+      );
+
+    printerConstructor.alignCenter();
+    printerConstructor.println('Testing Print');
+    printerConstructor.cut();
+
+    try {
+      await printerConstructor.execute();
+      console.error('Print Finish!');
+    } catch (error) {
+      console.log('Print Error:', error);
+    }
   }
-
-  let printer = null;
-  try {
-    printer = new ThermalPrinter({
-      type: PrinterTypes.TANCA,
-      interface: `TCP://${portName}`,
-    });
-  } catch (e) {
-    console.log('Failed to try to print');
-  }
-
-  if (!printer) return;
-
-  const isConnected = await printer.isPrinterConnected();
-  console.log('Status da impressora: ', isConnected);
-
-  if (!isConnected)
-    return console.log(
-      'Impressora desligada, portanto não foi possível imprimir!'
-    );
-
-  printer.alignCenter();
-  printer.println('Testing Print');
-  printer.cut();
-
-  try {
-    await printer.execute();
-    console.error('Print Finish!');
-  } catch (error) {
-    console.log('Print Error:', error);
-  }
-}
+})();
